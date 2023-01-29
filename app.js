5b2dcb0e22748baaa31052890a2398787819c157
Stevenrej <steveo732@gmail.com>
2023-01-29 13:19:29 -0600

    added Auth0 inital functionality

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
 
 const express = require('express');
 const http = require('http');
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 require('dotenv').config();
 const socketio = require('socket.io');
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const userRouter = require('./routes/userRoutes');
+// const userRouter = require('./routes/userRoutes');
 const postRouter = require('./routes/postRoutes');
 const taskRouter = require('./routes/taskRoutes');
 const mentorProtegeRouter = require('./routes/mentorProtegeRoutes');
@@ -23,6 +25,30 @@ if (process.env.NODE_ENV === 'test') {
 } else {
   DATABASE_URL = process.env.DATABASE_URL_LIVE;
 }
+
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  console.log(req);
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+
 
 app.use(express.json());
 app.use(cors());
@@ -44,7 +70,7 @@ connectToMongoDB();
 
 app.use(mentorProtegeRouter);
 app.use(taskRouter);
-app.use(userRouter);
+// app.use(userRouter);
 app.use(postRouter);
 startIo(io);
 app.all('/', (req, res, next) => {
