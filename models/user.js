30dafc8a6cff813ec8d20ef9a9dd53a711d7b30f
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-01-31 15:17:45 -0500

    add connection request to user route

--- models/user.js
+++ models/user.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    connection_requests: {
+      type: [String],
+      default: [],
+    },
     email: {
       type: String,
       required: false,
